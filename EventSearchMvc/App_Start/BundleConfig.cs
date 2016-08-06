9bd8b095781b23ffd35d9e8b4dddfb8f719d550c
Henry <henryma14@gmail.com>
2016-08-06 14:49:02 -0700

    Bug fixing

--- EventSearchMvc/App_Start/BundleConfig.cs
+++ EventSearchMvc/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace EventSearchMvc
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ EventSearchMvc/MyScripts/app.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="F:\Visual_Studio_2015_Apps\EventSearchMvc\EventSearchMvc\Scripts/angular.min.js" />
+
+'use strict'
+
+var app = angular.module('App', [])
+
+app.controller('AppController', ['$scope', '$filter', '$http', '$localStorage',
+    function ($scope, $filter, $http, $localStorage) {
+        //$scope.startDate = $filter('date')(new Date('01/01/2016'), 'yyyy-MM-dd')
+        //$scope.endDate = $filter('date')(new Date(), 'yyyy-MM-dd')
+
+        $scope.init = function (location, category, startDate, endDate) {
+            if (location !== undefined) {
+                $scope.location = location
+                $localStorage.location = location
+            }
+            else {
+                $scope.location = $localStorage.location
+            }
+            if (category !== undefined) {
+                $scope.category = category
+                $localStorage.category = category
+            }
+            else {
+                $scope.category = $localStorage.category
+            }
+
+        
+            $scope.startDate = startDate == null ? $filter('date')(new Date('01/01/2016'), 'yyyy-MM-dd') : startDate
+            $scope.endDate = endDate == null ?  $filter('date')(new Date(), 'yyyy-MM-dd') : endDate           
+        }
+    }])
